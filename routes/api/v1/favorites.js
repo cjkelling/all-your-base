fb36f9106436539301c77ba1790fb72083ec9016
Christopher Kelling <cj.kelling88@gmail.com>
2020-02-01 09:21:03 -0700

    Create post request to favorites file in routes

---
+++ routes/api/v1/favorites.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const router = express.Router();
+
+const environment = process.env.NODE_ENV || "development";
+const configuration = require("../../../knexfile")[environment];
+const database = require("knex")(configuration);
+
+router.post("/", (req, res) => {
+  const user_key = req.body.api_key;
+  const location = req.query.location;
+
+  database("users").where({api_key: user_key}).first()
+  .then(user => {
+    if(user_key != user.api_key){
+      res.status(401).send("Unauthorized")
+    }
+    else{
+      if(location === null || location === ''){
+        res.status(401).send("Enter a Valid Location")
+      }
+      else{
+        database('locations').insert({name: location, user_id: user.id})
+        .then(favorite => {
+          res.status(200).send(`${location} has been added to your favorites`)
+        })
+        .catch(error => {res.status(500).json({ error })});
+      }
+    }
+  })
+})
+
+module.exports = router;
