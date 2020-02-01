851faf7a61e9adb1906c682f79b77562ade3bf1b
Christopher Kelling <cj.kelling88@gmail.com>
2020-02-01 07:25:16 -0700

    Add location table and reference to user_id. Add seed data for locations.

--- db/migrations/20200128094719_first.js
+++ db/migrations/20200128094719_first.js
@@ -8,12 +8,22 @@ exports.up = function(knex) {
       table.string('api_key');
 
       table.timestamps(true, true);
+    }),
+
+    knex.schema.createTable('locations', function(table) {
+      table.increments('id').primary();
+      table.string('name');
+      table.integer('user_id').unsigned()
+      table.foreign('user_id').references('users.id');
+
+      table.timestamps(true, true);
     })
   ])
 };
 
 exports.down = function(knex) {
   return Promise.all([
+    knex.schema.dropTable('locations'),
     knex.schema.dropTable('users')
   ])
 };
