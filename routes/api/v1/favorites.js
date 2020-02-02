084738ebff6e707bdf1c1760511d17cca183c472
Christopher Kelling <cj.kelling88@gmail.com>
2020-02-02 10:56:57 -0700

    Favorites route now has get and delete methods that utilize helper methods

--- routes/api/v1/favorites.js
+++ routes/api/v1/favorites.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
+const current_weather = require('../../../helper_methods/current_weather');
+const google_geocoding = require('../../../helper_methods/google_geocoding');
+const darksky_forecast = require('../../../helper_methods/darksky_forecast');
+
 const environment = process.env.NODE_ENV || "development";
 const configuration = require("../../../knexfile")[environment];
 const database = require("knex")(configuration);
@@ -16,12 +20,12 @@ router.post("/", (req, res) => {
     }
     else{
       if(location === null || location === ''){
-        res.status(404).send(" Location Not Found. Please Enter a Valid Location.")
+        res.status(404).send("Please Enter a Valid Location.")
       }
       else{
         database('locations').insert({name: location, user_id: user.id})
         .then(favorite => {
-          res.status(200).send(`${location} has been added to your favorites`)
+          res.status(200).send(`${location} has been added to your favorites.`)
         })
         .catch(error => {res.status(500).json({ error })});
       }
@@ -39,14 +43,53 @@ router.get("/", (req, res) => {
     }
     else{
       database('locations').where({user_id: user.id}).select()
-      .then((location) => {
-        res.status(200).json(location);
+      .then(locations => {
+        locations.map(location => {
+          google_geocoding(location.name)
+          .then(lat_long => {
+            if(lat_long != null && lat_long != ''){
+              darksky_forecast(lat_long)
+              .then(weather_data => {
+                res.status(200).json((new current_weather(location, weather_data)).findForecast())
+              })
+              .catch((error) => console.error({ error }))
+            }
+            else{
+              res.status(401).send("Forecast Information Could Not Be Found.")
+            }
+          })
+        })
       })
       .catch((error) => {
         res.status(500).json({ error });
       });
     }
   })
+})
+
+router.delete("/", (req, res) => {
+  const user_key = req.body.api_key;
+  const location = req.body.location;
+
+  database("users").where({api_key: user_key}).first()
+  .then(user => {
+    if(user_key != user.api_key){
+      res.status(401).send("Unauthorized")
+    }
+    else{
+      if(location === null || location === ''){
+        res.status(404).send("Please Enter a Valid Location.")
+      }
+      else{
+        database('locations').where({name: location, user_id: user.id})
+        .then((location) => {
+          return delete(location)
+        })
+        res.status(200).send(`${location} has been removed from your favorites.`)
+      }
+    };
+  })
+  .catch(error => {res.status(500).json( "Location Not Favorited. Please Enter a Valid Location." )});
 })
 
 module.exports = router;
